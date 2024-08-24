216d38bdccc05f9541a078966ca9494979732d41
Hugo Abreu <hugo.abreu@fanduel.com>
2024-08-24 13:48:03 +0100

    chore(file): add file not found error

--- src/main/java/pt/blip/resources/FileResource.java
+++ src/main/java/pt/blip/resources/FileResource.java
@@ -1,10 +1,13 @@
 package pt.blip.resources;
 
+import io.quarkus.vertx.http.runtime.devmode.Json;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.apache.commons.io.FileUtils;
 import org.jboss.resteasy.reactive.RestPath;
 import pt.blip.api.AlphaFile;
@@ -18,10 +21,18 @@ public class FileResource {
     @GET
     @Path("{file}")
     @Produces(MediaType.APPLICATION_JSON)
-    public AlphaFile read(@RestPath final String file) throws IOException {
+    public AlphaFile read(@RestPath final String file) {
         final File readFile = new File(file);
-        String data = FileUtils.readFileToString(readFile, "UTF-8");
-        return new AlphaFile(file, data);
+        try {
+            final String data = FileUtils.readFileToString(readFile, "UTF-8");
+            return new AlphaFile(file, data);
+        } catch (final IOException ioException) {
+            throw new NotFoundException(
+                    Response.status(Response.Status.NOT_FOUND).entity(Json.object().put(
+                            "error", "File not found."
+                    ).build()).build()
+            );
+        }
     }
 
     @PUT
